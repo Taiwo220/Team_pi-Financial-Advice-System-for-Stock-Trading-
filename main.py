32ea81f80dac21402abe4d9bde1a7db8126fcef6
Alexin <alexindevs@gmail.com>
2025-03-04 17:19:00 +0100

    feat: add query system

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app.config.db import async_engine
 from app.models.base import Base
 from fastapi.middleware.cors import CORSMiddleware
 from app.jobs.scheduler import start_scheduler
+from app.routes import query
 from app.config.chromadb import close_chromadb
 import asyncio
 
@@ -30,6 +31,8 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
 )
+
+app.include_router(query.router, prefix="/api", tags=["Financial Advice"])
 
 @app.get("/")
 def read_root():
